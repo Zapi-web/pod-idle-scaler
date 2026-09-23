06143508c4ba6abd1d696c6cb41bf3b68685d567
Zapi <screbov2@gmail.com>
2026-09-23 18:14:11 +0200

    feat: define IdleScaler v1alpha1 types and triggers (#1)

--- api/v1alpha1/idlescaler_types.go
+++ api/v1alpha1/idlescaler_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -24,39 +25,63 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// IdleScalerPhase describe current lifecycle phase of the scaler
+// +kubebuilder:validation:Enum=Active;Idling;Sleeping;Error
+type IdleScalerPhase string
+
+const (
+	PhaseActive   IdleScalerPhase = "Active"
+	PhaseIdling   IdleScalerPhase = "Idling"
+	PhaseSleeping IdleScalerPhase = "Sleeping"
+	PhaseError    IdleScalerPhase = "Error"
+)
+
 // IdleScalerSpec defines the desired state of IdleScaler
 type IdleScalerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of IdleScaler. Edit idlescaler_types.go to remove/update
+	// Replicas to scale up to when waking up
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// Time before reduce
+	// +kubebuilder:default="15m"
+	// +optional
+	IdleTimeout *metav1.Duration `json:"idleTimeout,omitempty"`
+
+	// Link on Deployment
+	// +required
+	ScaleTargetRef autoscalingv2.CrossVersionObjectReference `json:"scaleTargetRef"`
+
+	// Minimal scaling number
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:default=0
+	ScaleMinimum *int32 `json:"scaleMinimum,omitempty"`
+
+	// Gets Activity metric from Custom/External Metrics API
+	// +required
+	Trigger TriggerSpec `json:"trigger"`
 }
 
 // IdleScalerStatus defines the observed state of IdleScaler.
 type IdleScalerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the IdleScaler resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
 	// The status of each condition is one of True, False, or Unknown.
 	// +listType=map
 	// +listMapKey=type
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// Number of ready replicas
+	// +optional
+	ReadyReplicas *int32 `json:"readyReplicas,omitempty"`
+
+	// Phase provides a summary of the scaller state
+	// +optional
+	Phase IdleScalerPhase `json:"phase,omitempty"`
+
+	// Last Activity Time
+	// +optional
+	LastActivityTime *metav1.Time `json:"lastActivityTime,omitempty"`
 }
 
 // +kubebuilder:object:root=true
